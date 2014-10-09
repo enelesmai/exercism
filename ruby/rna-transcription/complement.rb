4ca725187b59f40a33f00a4c56cedd0e08ac7707
enelesmai <xochitl.selene.fp@gmail.com>
2014-10-09 00:52:13 -0500

    using symbol instead of integer as types

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -3,28 +3,23 @@ class Complement
 	def self.complementing(strand,type)
 		strd = ""
 		nucleotides = { "G" => 'C', "C" => 'G', "T" => 'A', "U" => 'A'}
-		nucleotides["A"] = if type == 1
+		nucleotides["A"] = if type == :dna
 			'U'
 		else
 			'T'
 		end
-
-		if strand.length > 1
-			strand.each_char do |s|
-				strd << nucleotides[s]				
-			end
-		else
-			strd = nucleotides[strand]
+		strand.each_char do |s|
+			strd << nucleotides[s]				
 		end
 		return strd
 	end
 
 	def self.of_dna(dna_strand) 
-		complementing(dna_strand,1)
+		complementing(dna_strand,:dna)
 	end
 
 	def self.of_rna(rna_strand) 
-		complementing(rna_strand,2)
+		complementing(rna_strand,:rna)
 	end
 
 end
